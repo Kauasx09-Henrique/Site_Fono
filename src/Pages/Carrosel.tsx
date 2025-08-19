989b73345c13841023a5c58c19682b56ed2643ce
Kauã Henrique <kauahenriquesx09@gmail.com>
2025-08-19 17:22:04 -0300

    task

--- src/Pages/Carrosel.tsx
+++ src/Pages/Carrosel.tsx
@@ -4,7 +4,7 @@ import { Pagination, Navigation, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-import './style/Carrossel.css';
+import './style/Carrosel.css';
 import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
 
 const testimonialsData = [
